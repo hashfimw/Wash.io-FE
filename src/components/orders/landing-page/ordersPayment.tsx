c4d5fb6e733e1d7fe8d8d0004abc725a930a2da6
“fauzima” <fauzimakarimsiregar@gmail.com>
2025-03-10 03:02:22 +0700

    fixing midtrans

--- src/components/orders/landing-page/ordersPayment.tsx
+++ src/components/orders/landing-page/ordersPayment.tsx
@@ -68,7 +68,7 @@ const PaymentPage: React.FC = () => {
 
     const script = document.createElement("script");
     script.src = "https://app.sandbox.midtrans.com/snap/snap.js";
-    script.setAttribute("data-client-key", process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY || "");
+    script.setAttribute("data-client-key", process.env.MIDTRANS_CLIENT_KEY!);
     script.onload = () => console.log("Midtrans Snap loaded");
     document.body.appendChild(script);
   }, []);
